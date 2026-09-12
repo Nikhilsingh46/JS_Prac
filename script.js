5c9a7b1535d79a4be507b4f8801330f1496325b6
Nikhil kumar <143347074+Nikhilsingh46@users.noreply.github.com>
2026-09-12 16:56:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ let a = 10;
 }
 
 console.log("Outer", a);
+
+//question 1
+const name = "Nikhil";
+const city = "Hajipur";
+let age = 22;
+
+// question 2
+console.log("Answer of Question no 2 is we get an error because we can't redeclared let")
+
+//question 3
+console.log(count);
+var count = 42;
+
+//question 4
+
+const detail = {
+  Name : "nikhil",
+  city : "Hajipur",
+  age : 22
+
+}
+
+// Practice set of Data Type
+
+console.log(true + false);
+console.log(null + 1);
